468f7daa28f19111e1b8acada14da24e41302a53
Victor Oliveira <victor.oliveira@lambda3.com.br>
2023-04-14 10:01:05 -0300

    Commit Inicial

---
+++ Dominio/Entidades/Produto.cs
@@ -0,0 +1,8 @@
+﻿namespace Dominio.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Descricao{ get; set; }
+    }
+}
---
+++ Infraestrutura/Dados/Contexto.cs
@@ -0,0 +1,23 @@
+﻿using Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Infraestrutura.Dados
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Produto> Produto { get; set; }
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
