a2fad10bfb48fd88ed669933bb3be43c01bbb9ac
Victor <victor__campos@hotmail.com.br>
2023-10-02 19:11:16 -0300

    Changes

---
+++ Helpers/Operacoes.cs
@@ -0,0 +1,10 @@
+﻿namespace projeto_dotnet_testes.Helpers
+{
+    public static class Operacoes
+    {
+        public static int Adicionar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ Testes/TDD/HelpersTestes/OperacoesTestes.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using projeto_dotnet_testes.Helpers;
+
+namespace TDD.HelpersTestes
+{
+    public class OperacoesTestes
+    {
+        [TestCase(1, 2, 3)]
+        [TestCase(-5, 1, -4)]
+        public void Adiciona_DeveSomarDoisNumeros_Corretamente(int num1, int num2, int resultadoEsperado)
+        {
+            var resultado = Operacoes.Adicionar(num1, num2);
+
+            resultado.Should().Be(resultadoEsperado);
+        }
+    }
+}
